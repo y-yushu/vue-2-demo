2219da30715b09f527bc161c1f50f15d6aa90700
yyshu <yyshu@hisw.cn>
2024-08-27 18:28:25 +0800

    feat: 学习three

--- src/views/three/index.vue
+++ src/views/three/index.vue
@@ -1,76 +1,98 @@
 <template>
   <div class="three-page">
-    <canvas id="webglcanvas"></canvas>
-    <div @mousedown="startTrigger" @mouseup="stopTrigger" @mouseleave="stopTrigger">Hold me</div>
+    <div id="webglcanvas"></div>
   </div>
 </template>
 
 <script>
 import * as THREE from 'three'
-// import ImageMap from '../../assets/threeImage/map.png'
 
 export default {
   name: 'ThreePage',
   data() {
-    return {
-      animationFrameId: null,
-      isTriggering: false,
-      cube: null
-    }
+    return {}
   },
   mounted() {
-    console.log('THREE', THREE)
     this.init()
   },
   methods: {
     init() {
+      // 操作元素
       const canvas = document.getElementById('webglcanvas')
+      const width = canvas.offsetWidth
+      const height = canvas.offsetHeight
+      // 创建相机、元素、渲染器
       const scene = new THREE.Scene()
-      const camera = new THREE.PerspectiveCamera(75, canvas.offsetWidth / canvas.offsetHeight, 0.1, 1000)
+      scene.background = new THREE.Color(0x050505)
+      const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+      camera.position.z = 100
+      const renderer = new THREE.WebGLRenderer()
+      renderer.setSize(width, height)
 
-      const geometry = new THREE.BoxGeometry(1, 1, 1)
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-      const cube = new THREE.Mesh(geometry, material)
-      this.cube = cube
-      scene.add(cube)
-      camera.position.z = 5
-
-      const renderer = new THREE.WebGLRenderer({
-        canvas: canvas
+      const uniforms = {
+        amplitude: { value: 1.0 },
+        color: { value: new THREE.Color(0xff2200) },
+        colorTexture: { value: new THREE.TextureLoader().load('static/water.jpg') }
+      }
+      uniforms['colorTexture'].value.wrapS = uniforms['colorTexture'].value.wrapT = THREE.RepeatWrapping
+      const vertexshader = `uniform float amplitude;
+attribute float displacement;
+varying vec3 vNormal;
+varying vec2 vUv;
+void main() {
+  vNormal = normal;
+  vUv = ( 0.5 + amplitude ) * uv + vec2( amplitude );
+  vec3 newPosition = position + amplitude * normal * vec3( displacement );
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+}`
+      const fragmentshader = `varying vec3 vNormal;
+varying vec2 vUv;
+uniform vec3 color;
+uniform sampler2D colorTexture;
+void main() {
+  vec3 light = vec3( 0.5, 0.2, 1.0 );
+  light = normalize( light );
+  float dProd = dot( vNormal, light ) * 0.5 + 0.5;
+  vec4 tcolor = texture2D( colorTexture, vUv );
+  vec4 gray = vec4( vec3( tcolor.r * 0.3 + tcolor.g * 0.59 + tcolor.b * 0.11 ), 1.0 );
+  gl_FragColor = gray * vec4( vec3( dProd ) * vec3( color ), 1.0 );
+}`
+      const shaderMaterial = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        vertexShader: vertexshader,
+        fragmentShader: fragmentshader
       })
-      renderer.setSize(canvas.offsetWidth, canvas.offsetHeight)
-      const animate = () => {
-        requestAnimationFrame(animate)
-        // cube.rotation.x += 0.01
-        // cube.rotation.y += 0.01
+      const radius = 50,
+        segments = 128,
+        rings = 64
+      const geometry = new THREE.SphereGeometry(radius, segments, rings)
+      const displacement = new Float32Array(geometry.attributes.position.count)
+      let noise = new Float32Array(geometry.attributes.position.count)
+      for (let i = 0; i < displacement.length; i++) {
+        noise[i] = Math.random() * 5
+      }
+      geometry.setAttribute('displacement', new THREE.BufferAttribute(displacement, 1))
+      const sphere = new THREE.Mesh(geometry, shaderMaterial)
+      scene.add(sphere)
+      const render = () => {
+        const time = Date.now() * 0.01
+        sphere.rotation.y = sphere.rotation.z = 0.01 * time
+        uniforms['amplitude'].value = 2.5 * Math.sin(sphere.rotation.y * 0.125)
+        uniforms['color'].value.offsetHSL(0.0005, 0, 0)
+        for (let i = 0; i < displacement.length; i++) {
+          displacement[i] = Math.sin(0.1 * i + time)
+          noise[i] += 0.5 * (0.5 - Math.random())
+          noise[i] = THREE.MathUtils.clamp(noise[i], -5, 5)
+          displacement[i] += noise[i]
+        }
+        sphere.geometry.attributes.displacement.needsUpdate = true
         renderer.render(scene, camera)
       }
-      animate()
-    },
-    triggerAction() {
-      console.log('Action triggered')
-      // 在这里添加你希望持续执行的逻辑
-      if (this.cube) {
-        this.cube.rotation.x += 0.03
-        this.cube.rotation.y += 0.03
+      const animate = () => {
+        render()
       }
-      // 继续下一个动画帧
-      if (this.isTriggering) {
-        this.animationFrameId = requestAnimationFrame(this.triggerAction)
-      }
-    },
-    startTrigger() {
-      // 开始持续触发
-      this.isTriggering = true
-      this.triggerAction() // 启动动画循环
-    },
-    stopTrigger() {
-      // 停止持续触发
-      this.isTriggering = false
-      if (this.animationFrameId) {
-        cancelAnimationFrame(this.animationFrameId)
-        this.animationFrameId = null
-      }
+      renderer.setAnimationLoop(animate)
+      canvas.appendChild(renderer.domElement)
     }
   }
 }
@@ -78,11 +100,12 @@ export default {
 
 <style lang="less" scoped>
 .three-page {
+  position: relative;
   padding: 30px;
 }
 #webglcanvas {
+  position: relative;
   width: 100%;
   aspect-ratio: 3 / 1;
-  background: pink;
 }
 </style>
